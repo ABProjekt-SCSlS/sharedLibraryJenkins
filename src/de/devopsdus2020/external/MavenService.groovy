3f01693898ec8bf5019c6a27076938fd1c51923d
Mostafa <m.torbon@gmx.de>
2020-10-15 16:29:17 +0200

    Added user & password for nexus

--- src/de/devopsdus2020/external/MavenService.groovy
+++ src/de/devopsdus2020/external/MavenService.groovy
@@ -59,6 +59,6 @@ class MavenService implements InterfaceMavenService {
     }
 
     Integer deploy(Map config) {
-        return this.executeMaven(config, "clean deploy --settings=${config.WORKSPACE}/settings.xml -DskipTests")
+        return this.executeMaven(config, "clean deploy --settings=${config.WORKSPACE}/settings.xml -Drepo.login=${config.NEXUS_USER} -Drepo.pwd=${config.NEXUS_PASSWORD} -DskipTests")
     }
 }
